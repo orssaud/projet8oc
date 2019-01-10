022a0f6abe60d4d429a149c17b045f3192888f73
orssaud <46528832+orssaud@users.noreply.github.com>
2019-01-10 10:31:19 +0100

    Revert "Revert "Suppression de fichier doublon""
    This reverts commit 1b1b0b4209461a0adf2ef58bb932cdbeea22d89f.

--- controller/controller.php
+++ controller/controller.php
@@ -27,7 +27,7 @@ function post($id)
 	
 	$postManager = new \projet8\postManager();
 	$commentsManager = new \projet8\commentsManager();
-	$time = new \projet8\time();
+	$time = new \projet8\time(); // relative time
 
 	$post = $postManager->onePost($id);
 	if($post){
